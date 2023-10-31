a00bdd178ed2121aeec73be0bf53cd1addf8d422
Edis Tirei <edtireli@gmail.com>
2023-10-31 12:00:43 +0100

    restructing and refactoring of superfluous files into a more unified approach

--- modules/__init__.py
+++ modules/__init__.py
@@ -8,3 +8,10 @@ __version__ = "0.1.0"
 __author__ = "Edis Devin Tireli"
 __affiliation__ = "Copenhagen University"
 __email__ = "edis.devin.tireli@regionh.dk"
+
+# modules/__init__.py
+from .start import *
+from .par2nii import *
+from .start import *
+from .fonts import *
+from config import *
