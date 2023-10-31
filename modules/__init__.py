c0049cc35502b43b2a8294c96f63d2a3366ba8f4
Edis Tirei <edtireli@gmail.com>
2023-10-31 20:23:41 +0100

    addons

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,7 +4,7 @@ p-Brain: Advanced Neuroimaging Analysis Tool
 This package provides functionalities for in-depth analysis of .PAR/.REC MRI data, including...
 """
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = "Edis Devin Tireli"
 __affiliation__ = "Copenhagen University"
 
