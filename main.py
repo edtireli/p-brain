e6299e25ef93584bb08868dde7416721341c2be8
Edis Tirei <edtireli@gmail.com>
2023-10-31 14:34:31 +0100

    fixed exit/wrapper

--- main.py
+++ main.py
@@ -9,31 +9,32 @@ def main():
     parrec2nifti(data_directory, nifti_directory)
    
     #Welcome screen
-    welcome_screen()
-    choice = welcome_screen_choice()
+    while True:
+        welcome_screen()
+        choice = welcome_screen_choice()
 
-    #choices
-    if choice == 0: #Show MRI images: DCE, Saggital T1/T2, Axial T1/T2
-        viewer = MRIViewer(nifti_directory)
-        viewer.display()
-    #elif choice == 1:
-        #do something 
-    #elif choice == 2:
-        #do something 
-    #elif choice == 3:
-        #do something 
-    #elif choice == 4:
-        #do something
-    #elif choice == 5:
-        #do something 
-    #elif choice == 6:
-        #do something  
-    #elif choice == 7:
-        #do something
-    #elif choice == 8:
-        #do something
-    #elif choice == 9:
-        #do something       
+        #choices
+        if choice == 0: #Show MRI images: DCE, Saggital T1/T2, Axial T1/T2
+            viewer = MRIViewer(nifti_directory)
+            viewer.display()
+        #elif choice == 1:
+            #do something 
+        #elif choice == 2:
+            #do something 
+        #elif choice == 3:
+            #do something 
+        #elif choice == 4:
+            #do something
+        #elif choice == 5:
+            #do something 
+        #elif choice == 6:
+            #do something  
+        #elif choice == 7:
+            #do something
+        #elif choice == 8:
+            #do something
+        elif choice == 9:
+            break       
 
 if __name__ == '__main__':
     main()
