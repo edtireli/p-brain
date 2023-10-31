dd18d06f883e9bf8656e6e0f3ebe5eb0a0667c8e
Edis Tirei <edtireli@gmail.com>
2023-10-31 10:57:58 +0100

    adding a bunch of folders

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+"""
+p-Brain: Advanced Neuroimaging Analysis Tool
+
+This package provides functionalities for in-depth analysis of .PAR/.REC MRI data, including...
+"""
+
+__version__ = "0.1.0"
+__author__ = "Edis Devin Tireli"
+__affiliation__ = "Copenhagen University"
+__email__ = "edis.devin.tireli@regionh.dk"
