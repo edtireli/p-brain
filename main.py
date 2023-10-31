55157074dcb212462a49685f5b86e139b4893afe
Edis Tirei <edtireli@gmail.com>
2023-10-31 13:27:53 +0100

    added choice 0 - image viewing

--- main.py
+++ main.py
@@ -2,30 +2,37 @@ from config.settings import *
 from modules import *
 
 def main():
-    #Handle log_number choice and welcome screen choice
+    
+    #Handle log_number choice and data directories
     log_number = select_log_number()
-    setup_directories(log_number)
+    data_directory, analysis_directory, nifti_directory, image_directory = setup_directories(log_number)
+    parrec2nifti(data_directory, nifti_directory)
+   
+    #Welcome screen
     welcome_screen()
     choice = welcome_screen_choice()
-    if choice == 0:
+
+    #choices
+    if choice == 0: #Show MRI images: DCE, Saggital T1/T2, Axial T1/T2
+        viewer = MRIViewer(nifti_directory)
+        viewer.display()
+    #elif choice == 1:
+        #do something 
+    #elif choice == 2:
+        #do something 
+    #elif choice == 3:
+        #do something 
+    #elif choice == 4:
         #do something
-    elif choice == 1:
+    #elif choice == 5:
         #do something 
-    elif choice == 2:
-        #do something 
-    elif choice == 3:
-        #do something 
-    elif choice == 4:
-        #do something
-    elif choice == 5:
-        #do something 
-    elif choice == 6:
+    #elif choice == 6:
         #do something  
-    elif choice == 7:
+    #elif choice == 7:
         #do something
-    elif choice == 8:
+    #elif choice == 8:
         #do something
-    elif choice == 9:
+    #elif choice == 9:
         #do something       
 
 if __name__ == '__main__':
