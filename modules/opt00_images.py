333a080f80340d25634d50ff7d9063c6f859921c
Edis Tirei <edtireli@gmail.com>
2023-11-04 13:55:30 +0100

    small bugfixes

--- modules/opt00_images.py
+++ modules/opt00_images.py
@@ -24,7 +24,6 @@ class MRIViewer:
 
 
     def display(self):
-        # Separate list generation and choice from image display
         patterns_and_names = [
             ([r'WIPcs_T1W_3D_TFE_32channel\.nii'], 'Saggital T1'),
             ([r'ax([-_ ])?vwipcs_t1w_3d_tfe_32channel\.nii'], '3D Axial T1'),
@@ -38,49 +37,53 @@ class MRIViewer:
 
         while True:  # Outer loop to restart the choice
             print("------------------------------")
-            available_files = []
+            available_files = {}
             for i, (patterns, name) in enumerate(patterns_and_names):
                 filename = self.find_matching_file(patterns)
                 if filename:
+                    available_files[i + 1] = (filename, name)
                     print(f"| {colored(i + 1, 'cyan')} | {colored(name, 'green')}")
-                    available_files.append((filename, name))
                 else:
                     print(f"| {colored(i + 1, 'cyan')} | {colored(name, 'red')}")
             print(f"| {colored('q', 'red')} | {colored('Exit', 'red')}")
             print("------------------------------")
 
-            while True:  # Outer loop for the user choice
-                choice = input("Select an option or 'q' to quit: ")
-                if choice == 'q':
-                    return  # Exit the program
-                    
-                try:
-                    choice = int(choice) - 1
+            choice = input("Select an option or 'q' to quit: ")
+            if choice == 'q':
+                return  # Exit the program
+
+            try:
+                choice = int(choice)
+                if choice in available_files:
                     selected_file, selected_name = available_files[choice]
-                    
+
                     # Load the NIFTI file
                     img = nib.load(os.path.join(self.nifti_directory, selected_file))
                     self.img_data = img.get_fdata()
                     self.num_slices = self.img_data.shape[2]
                     self.num_frames = self.img_data.shape[3] if self.img_data.ndim == 4 else None
 
-                    while True:  # Inner loop for the GUI
+                    # Inner loop for the GUI
+                    while True:
                         # Create a new figure and axis
                         self.fig, self.ax = plt.subplots()
-                        
+
                         # Connect to events and show the figure
                         self.fig.canvas.mpl_connect('key_press_event', self.on_key)
                         self.redraw()
                         plt.show()
-                        
+
                         # Close the GUI and break the inner loop when 'Escape' is pressed
                         if plt.get_fignums():
                             continue  # Reopen the GUI if it is still open
                         else:
                             break  # Break the inner loop to return to the user choice
-                        
-                except (ValueError, IndexError):
+
+                else:
                     print("Invalid choice. Please try again.")
+            except ValueError:
+                print("Invalid input. Please enter a number or 'q' to quit.")
+
 
 
 
